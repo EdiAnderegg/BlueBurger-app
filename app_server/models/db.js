89942658b5a3c8daca8aa09acfbb9751998a4bd6
EdiAnderegg <93870106+EdiAnderegg@users.noreply.github.com>
2023-03-05 19:10:08 +0100

    integrated mongodb

---
+++ app_server/models/db.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose'); 
+const readLine = require('readline');
+
+if(process.platform === 'win32'){
+    const rl = readLine.createInterface ({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT',()=>{
+        process.emit('SIGINT');
+    })
+}
+
+
+const dbURI = 'mongodb://localhost/BlueBurger';
+mongoose.connect(dbURI,{useNewUrlParser: true});
+
+
+mongoose.connection.on('connected', ()=> {
+    console.log(`Mongoose connected to ${dbURI}`);
+})
+
+mongoose.connection.on('error', (err)=> {
+    console.error(`Mongoose connection error: `,err);
+})
+
+mongoose.connection.on('disconnected', ()=> {
+    console.log(`Mongoose disconnected`);
+})
+
+const gracefulShutdown = (msg, callback) => {
+    mongoose.connection.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    });
+}
+
+// For nodemon restarts 
+process.once('SIGUSR2', ()=> {
+    gracefulShutdown('nodemon restart', ()=>{
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+//For app termination
+process.on('SIGINT', ()=> {
+    gracefulShutdown('app termination', ()=>{
+        process.exit(0);
+    });
+});
+
+
+
+
+
+
+
